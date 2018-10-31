97ea6559f4f5d316bd1c544a515c29b5cf4ac327
Arsalan <z5163778@ad.unsw.edu.au>
2018-10-31 19:41:16 +1100

    chaining promises in advance_promise

--- playground/advance_promise.js
+++ playground/advance_promise.js
@@ -1,16 +1,41 @@
 
+var asyncAdd = (a, b)=>{ //this is just a wrapper function for promise()
+  return new Promise((resolve, reject)=>{  //new promise() @1 arg
+    setTimeout(()=>{
+        if(typeof a === 'number' && typeof b === 'number'){
+          resolve(a+b);
+        }else{
+          reject('Both args must be numbers.');
+        }
+    },2000);
+  });
+}
 
 
-var somePromise = new Promise((resolve, reject)=>{
-  //whichever comes first, will fire and it will fire only once! <promise is simply that>
-  resolve('Worked.');
-  reject('Not Worked!');
-
-});
-
-
-somePromise.then((message)=>{//on Fullfill
-  console.log('Success: ',message);
-},(errorMessage)=>{//on rejected
-  console.log('Failed: ', errorMessage);
+//we getting the result of 1 promise and passing it to the next one.
+asyncAdd(1,1)
+.then((result)=>{
+  console.log(result);
+  return asyncAdd(1,result);
 })
+.then((result)=>{
+  console.log(result);
+  return asyncAdd(2,result);
+})
+.then((result)=>{
+  console.log(result);
+  return asyncAdd(3,result);
+})
+.then((result)=>{
+  console.log(result);
+  return asyncAdd(5,result);
+})
+.then((result)=>{
+  console.log(result);
+  return asyncAdd(8,result);
+}).catch((errorMessage)=>{
+  console.log(errorMessage);
+})
+
+//when using chaining promises, we have to use just resolve cases in then()
+//and at the end, use just 1 .catch for all the errorMessages that would occured. Otherwise it goes on and crashes.
